1a659ecefea3020a5eeb4469352b836d5e6606e3
lukasbaumgartner <lukasfbaumgartner@gmail.com>
2026-02-02 14:48:14 +0100

    Begining of hw

---
+++ Homework 2.py
@@ -0,0 +1,19 @@
+
+# Task given the gross profit of an individual find the net profit
+
+# Lets begin by allowing the user to input their gross profit to find net profit
+
+input("Let us calculate your net profit, please enter your current gross profit")
+gross profit =
+if<1000:
+    print(income tax)
+
+
+elif money > 10**4:
+    print("Technically not poor")
+else:
+    print("I am sorry")
+
+if money< 10**4:
+    print("poor")
+    elif money< 10**5 and money > 10**4:
